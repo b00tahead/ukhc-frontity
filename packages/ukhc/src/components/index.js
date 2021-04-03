847e9f55ea17fce7fb48912a26e15b7a4bdd2847
Scott May <smay44@gmail.com>
2021-04-03 17:30:11 -0400

    create separate components for posts and pages

--- packages/ukhc/src/components/index.js
+++ packages/ukhc/src/components/index.js
@@ -5,6 +5,8 @@ import Link from "@frontity/components/link";
 import Switch from "@frontity/components/switch";
 
 import List from "./list";
+import Post from "./post";
+import Page from "./page";
 
 const Root = ({ state }) => {
   const data = state.source.get(state.router.link);
@@ -24,8 +26,8 @@ const Root = ({ state }) => {
       <main>
         <Switch>
           <List when={data.isArchive} />
-          <div when={data.isPost}>This is a post</div>
-          <div when={data.isPage}>This is a page</div>
+          <Post when={data.isPost} />
+          <Page when={data.isPage} />
         </Switch>
       </main>
     </>
